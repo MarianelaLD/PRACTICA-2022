4a10846156965b1d26b57bf3fad9704edff64249
XhydrargyrumX <71038037+XhydrargyrumX@users.noreply.github.com>
2022-01-13 19:38:36 -0300

    peos de la mane

--- scraping.py
+++ scraping.py
@@ -3,11 +3,13 @@ from operator import itemgetter
 import datetime
 import pandas as pd
 import requests
+import urllib3
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+http = urllib3.PoolManager(cert_reqs = 'CERT_NONE')
 url = "https://www.gendarmeria.gob.cl/estadisticaspp.html"
 pagina = requests.get(url)
 pagina_soup = BeautifulSoup (pagina.content, 'html.parser')
-parrafos = pagina_soup.findAll('p')
 tablas = pagina_soup.findAll('table')
 ultima_actualizacion = datetime.datetime.now()
 datos = []
@@ -17,35 +19,32 @@ meses_dict = []
 for i in range(1,13):
     mes = {'numero':i, 'nombre':nombres[i-1]}
     meses_dict.append(mes)
+aux = {}
 
-for parrafo in parrafos:
-    anclas = parrafo.findAll('a')
+for tabla in tablas:
+    año = int(tabla.find('td').text.split(' ')[-1])
+    anclas = tabla.findAll('a')
+    
     for ancla in anclas:
-        if 'Estadística' in ancla.text:
-            subsistema = ancla.text.split(' ')[-1]
-            for tabla in tablas:
-                año = int(tabla.find('td').text.split(' ')[-1])
-                anclas = tabla.findAll('a')
-                for h in anclas:
-                    link = h.get('href')
-                    mes = h.text
-                    mes = [i for i in meses_dict if mes in i['nombre']][0].get('numero')
-                    mydict = {'link': link, 'subsistema': subsistema, 'mes': mes, 'año': año}
-                    datos.append(mydict)
+        link = ancla.get('href')
+        subsistema = link.split('/')[-1].split('.')[1].split('_')[0]
+        mes = ancla.text
+        mes = [i for i in meses_dict if mes in i['nombre']][0].get('numero')
+        mydict = {'link': link, 'subsistema': subsistema, 'mes': mes, 'año': año}
+        datos.append(mydict)
+            
+cerrado = 0
+abierto = 0
+postpenitenciario = 0
 datos = sorted(datos, key=itemgetter('subsistema','año','mes'))
-a_limpiar = []
-link = datos[0]['link']
-link = link[:4] + link[5:]
-archivo = link.split('/')[-1]
 
-
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-http = urllib3.PoolManager(cert_reqs = 'CERT_NONE')
-excel = http.request('GET', link)
-
-try :
-    df = pd.read_excel(excel.text)
-    print(df)
-except:
-    print('No se pudo descargar el excel')
+for dato in datos:
+    
+    if dato.get('subsistema') == 'cerrado':
+       cerrado+=1
+       
+    elif dato.get('subsistema') == 'abierto':
+        abierto+=1
+    
+    elif dato.get('subsistema') == 'postpenitenciario':
+        postpenitenciario+=1
