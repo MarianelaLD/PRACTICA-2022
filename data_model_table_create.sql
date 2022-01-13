ce9d6b9865e3b2cc03c6bb8c6068e016da304ca3
Marianela Lara <manee.ld@gmail.com>
2022-01-13 18:52:13 -0300

    tables

---
+++ data_model_table_create.sql
@@ -0,0 +1,107 @@
+CREATE TABLE public.usuario (
+    rut  NOT NULL,
+    fecha_registro  NOT NULL,
+    ultimo_inicio  NOT NULL,
+    esta_eliminado  NOT NULL,
+    sesion_iniciada  NOT NULL,
+    PRIMARY KEY (rut)
+);
+
+
+CREATE TABLE public.informante (
+    rut_informante  NOT NULL,
+    nombre_informante  NOT NULL,
+    apellido_informante  NOT NULL,
+    id_verificacion  NOT NULL,
+    PRIMARY KEY (rut_informante, id_verificacion)
+);
+
+
+CREATE TABLE public.administrador (
+    id_administrador  NOT NULL,
+    nombres_administrador  NOT NULL,
+    apellidos_administrador  NOT NULL,
+    PRIMARY KEY (id_administrador)
+);
+
+
+CREATE TABLE public.verificador (
+    id_verificador  NOT NULL,
+    PRIMARY KEY (id_verificador)
+);
+
+
+CREATE TABLE public.locacion (
+);
+
+
+CREATE TABLE public.entity1 (
+);
+
+
+CREATE TABLE public.entity2 (
+);
+
+
+CREATE TABLE public.entity3 (
+);
+
+
+CREATE TABLE public.entity4 (
+);
+
+
+CREATE TABLE public. (
+);
+
+
+CREATE TABLE public.region (
+    id_region  NOT NULL,
+    nombre_region  NOT NULL,
+    PRIMARY KEY (id_region)
+);
+
+
+CREATE TABLE public.comuna (
+    id_comuna  NOT NULL,
+    nombre_comuna  NOT NULL,
+    PRIMARY KEY (id_comuna)
+);
+
+
+CREATE TABLE public.calle (
+    id_calle  NOT NULL,
+    nombre_calle  NOT NULL,
+    PRIMARY KEY (id_calle)
+);
+
+
+CREATE TABLE public.direccion (
+    id_direccion  NOT NULL,
+    numero_direccion  NOT NULL,
+    PRIMARY KEY (id_direccion)
+);
+
+
+CREATE TABLE public.verificacion (
+    id_verificacion  NOT NULL,
+    estado_verificacion  NOT NULL,
+    fecha_emision  NOT NULL,
+    fecha_modificacion  NOT NULL,
+    PRIMARY KEY (id_verificacion)
+);
+
+
+CREATE TABLE public.datos_verificables (
+    rut_verificable  NOT NULL,
+    nombre_verificable  NOT NULL,
+    apellido_verificable  NOT NULL,
+    id_direccion  NOT NULL,
+    estado_civil  NOT NULL,
+    estado_causa  NOT NULL,
+    motivo  NOT NULL,
+    cantidad_hijos  NOT NULL,
+    ficha_judicial  NOT NULL,
+    PRIMARY KEY (rut_verificable, id_direccion)
+);
+
